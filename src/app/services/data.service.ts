3645ca55cb18d70ae536a1bbdc3906b001765771
otitoju1 <john.o@allprimetech.com>
2022-02-10 23:56:58 -0800

    change api

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { usersData, User } from './data';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DataService {
-  private baseUrl = "https://jsonplaceholder.typicode.com/users"
+  private baseUrl = `${environment.apiUrl} + /api/v1/users`
 
   constructor(private http: HttpClient) { }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8000"
+  apiUrl: "https://otitoju-server.herokuapp.com"
 };
 
 /*
