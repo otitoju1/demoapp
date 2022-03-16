463db9af26888975ddac4f74e1934dd2a147ca48
otitoju1 <john.o@allprimetech.com>
2022-03-16 03:12:21 -0700

    Update disable-role.directive.ts

--- src/app/directives/disable-role.directive.ts
+++ src/app/directives/disable-role.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { TokenStorageService } from '../services/tokenStorage.service';
 
 @Directive({
   selector: '[appDisableRole]'
 })
 export class DisableRoleDirective {
+  @Input() disableRole: string;
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenStorageService,
+    private renderer: Renderer2,  // make effect on the style of the element
+    private elementRef: ElementRef
+  ) { }
 
+  ngAfterViewInit() {
+    this.tokenService.getUser().subscribe( user => {
+      
+    })
+  }
 }
